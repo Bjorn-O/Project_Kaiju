2b91fb0ce3b440c4d051cb7f7143e537080f1b75
Björn <Bjorn.oudkerk@outlook.com>
2023-04-25 16:45:06 +0200

    Stable Fire Rate, effected added needs updated. Needs damage logic

--- Unity Project- Kaiju/Assets/Scripts/Gameplay/Gun/GunFire.cs
+++ Unity Project- Kaiju/Assets/Scripts/Gameplay/Gun/GunFire.cs
@@ -2,13 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+using UnityEngine.VFX;
 
 public class GunFire : MonoBehaviour
 {
-    [SerializeField] private float fireRate;
+    [FormerlySerializedAs("shootAble")] [SerializeField] private LayerMask shootAbleLayer;
+    [SerializeField] private Transform muzzlePoint;
+    [SerializeField] private VisualEffect shootEffect;
+    
+    [SerializeField] private float fireRatePerSecond;
+
+    private RaycastHit _hit;
+    private int i;
 
     private void OnFire(InputValue value)
     {
-        print(value.Get<float>());
+        var isPressed = value.Get<float>();
+
+        if (isPressed > 0)
+        {
+            InvokeRepeating(nameof(Pew), 0, (60 / fireRatePerSecond) / 100);
+            
+            print(60 / fireRatePerSecond / 100);
+        }
+        else CancelInvoke(nameof(Pew));
+        
+
+    }
+
+    private void Pew()
+    {
+        shootEffect.Play();
+        if (Physics.Raycast(muzzlePoint.position, muzzlePoint.forward, out _hit, shootAbleLayer))
+        {
+            
+        }
+        //Play Sound effect
+        //Debug.DrawRay(muzzlePoint.position, muzzlePoint.forward, Color.red);
     }
 }
