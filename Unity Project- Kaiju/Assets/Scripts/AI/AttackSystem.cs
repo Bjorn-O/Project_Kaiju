70680c69e376956d32580c8c27547d772b7a6343
Jorikos <26204@ma-web.nl>
2023-04-26 14:04:48 +0200

    Added support for multiple colliders

--- Unity Project- Kaiju/Assets/Scripts/AI/AttackSystem.cs
+++ Unity Project- Kaiju/Assets/Scripts/AI/AttackSystem.cs
@@ -6,26 +6,49 @@ public class AttackSystem : MonoBehaviour
     [SerializeField] private GameObject floorMarkerObject;
     [SerializeField] private Vector3 storePosition;
     [SerializeField] private GameObject boatObject;
+    [SerializeField] private Sprite[] floorMarkers;
+    [SerializeField] private MeshCollider[] attackColliders;
+
+    private MeshCollider usedCollider;
     private Quaternion rotation;
-    private CapsuleCollider damageTrigger;
     private SpriteRenderer markerRenderer;
 
     private void Awake()
     {
-        damageTrigger = floorMarkerObject.GetComponent<CapsuleCollider>();
         markerRenderer = floorMarkerObject.GetComponent<SpriteRenderer>();
         rotation = Quaternion.Euler(90, 0, 0);
     }
-    
-    public IEnumerator FloorMarkerAttack(Vector3 position, float timeToWait, float yScale = default)
+
+    private void Start()
     {
-        floorMarkerObject.transform.position = position;
-        floorMarkerObject.transform.rotation = rotation;
+        StartCoroutine(FloorMarkerAttack(new Vector3(0, 10, 10), boatObject.transform.position, 5, 1));
+    }
+
+
+    /// <summary>
+    /// Creates floor marker and checks if players are inside it.
+    /// </summary>
+    /// <param name="spawnPosition">Spawn position of floor marker.</param>
+    /// <param name="objectToFace">Which object does it face.</param>
+    /// <param name="timeToWait">Time before damage is applied.</param>
+    /// <param name="sprite">Sprite and corresponding collider to use on floor marker.</param>
+    public IEnumerator FloorMarkerAttack(Vector3 spawnPosition, Vector3 objectToFace, float timeToWait, int sprite)
+    {
+        markerRenderer.sprite = floorMarkers[sprite];
+        usedCollider = attackColliders[sprite];
+        floorMarkerObject.transform.position = spawnPosition;
+
+        objectToFace.x = 90;
+        objectToFace.z = 0;
+
+        floorMarkerObject.transform.LookAt(objectToFace);
+
         yield return new WaitForSeconds(timeToWait);
         markerRenderer.enabled = false;
 
-        if (Vector3.Distance(boatObject.transform.position, damageTrigger.transform.position) < damageTrigger.radius) //Checks if boat is in trigger radius
+        if (usedCollider.bounds.Contains(boatObject.transform.position)) //Checks if boat is in trigger radius
         {
+            Debug.Log("Hit");
             //Damage function
         }
 
