f6cb5fb91d3998c9e6e8d71e8e05dd4faf57209c
Björn <Bjorn.oudkerk@outlook.com>
2023-05-01 17:16:49 +0200

    Added Path Class. Bezier Curve Logic
    The Path Class can be created and used to keep track between the last and current point and create a curve accordingly

---
+++ Unity Project- Kaiju/Assets/Scripts/Systems/Waypoints/PathTester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace Systems.Waypoints
+{
+    public class PathTester : MonoBehaviour
+    {
+        public Transform startTransform;
+        public Transform endTransform;
+
+
+        private Vector3 lastPos;
+        private BezierPath _currentBezierPath;
+
+        private void Start()
+        {
+            _currentBezierPath = BezierPath.CreateInstance(startTransform, endTransform);
+        }
+
+        private void Update()
+        {
+            _currentBezierPath.UpdatePath(startTransform, endTransform);
+            
+            for (float i = 0; i <= 1; i += 0.0625f)
+            {
+                Debug.DrawLine(lastPos, _currentBezierPath.ReturnPosition(i));
+                lastPos = _currentBezierPath.ReturnPosition(i);
+                if (i == 1)print(lastPos);
+            }
+        }
+    }
+}
--- Unity Project- Kaiju/Assets/Scripts/Systems/Waypoints/RailMovement.cs
+++ Unity Project- Kaiju/Assets/Scripts/Systems/Waypoints/RailMovement.cs
@@ -6,31 +6,40 @@ using UnityEngine;
 
 public class RailMovement : MonoBehaviour
 {
-    public Waypoints wp;
-    //private Transform currentWaypoint;
     public int i;
     public int totalWaypoints;
     public float speed;
+    
+    private Waypoint _lastWaypoint;
+    private Waypoint _currentWaypoint;
+    
     private void Start()
     {
         i = 0;
     }
 
+    private void UpdatePath(Waypoint nextWaypoint)
+    {
+        _lastWaypoint = _currentWaypoint;
+        _currentWaypoint = nextWaypoint;
+        
+    }
+
     private void Update()
     {
         
-        if(Vector3.Distance(transform.position, wp.waypoints[i].transform.position) == 0f)
-        {
-            i++;
-            
-            print(i);
-        }
-        if(i >= totalWaypoints)
-        {
-            i = 0;
-        }
-        print("Current Waypoint coörds = " + wp.waypoints[i].transform.position);
-        transform.LookAt(wp.waypoints[i].transform);
-        transform.position = Vector3.MoveTowards(transform.position, wp.waypoints[i].transform.position, speed * Time.deltaTime);
+        // if(Vector3.Distance(transform.position, wp.waypoints[i].transform.position) == 0f)
+        // {
+        //     i++;
+        //     
+        //     print(i);
+        // }
+        // if(i >= totalWaypoints)
+        // {
+        //     i = 0;
+        // }
+        // print("Current Waypoint coörds = " + wp.waypoints[i].transform.position);
+        // transform.LookAt(wp.waypoints[i].transform);
+        // transform.position = Vector3.MoveTowards(transform.position, wp.waypoints[i].transform.position, speed * Time.deltaTime);
     }
 }
