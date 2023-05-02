16c395650d42533fefc5e0419907f5325b5f1dd0
DOOT <26204@ma-web.nl>
2023-05-02 12:20:43 +0200

    Finished

--- Unity Project- Kaiju/Assets/Scripts/AI/AttackSystem.cs
+++ Unity Project- Kaiju/Assets/Scripts/AI/AttackSystem.cs
@@ -9,19 +9,16 @@ public class AttackSystem : MonoBehaviour
     [SerializeField] private Sprite[] floorMarkers;
     [SerializeField] private MeshCollider[] attackColliders;
 
+    private bool isCancelled;
     private MeshCollider usedCollider;
-    private Quaternion rotation;
     private SpriteRenderer markerRenderer;
+
+    public bool IsCancelled { get; set; }
 
     private void Awake()
     {
+        isCancelled = false;
         markerRenderer = floorMarkerObject.GetComponent<SpriteRenderer>();
-        rotation = Quaternion.Euler(90, 0, 0);
-    }
-
-    private void Start()
-    {
-        StartCoroutine(FloorMarkerAttack(new Vector3(0, 10, 10), boatObject.transform.position, 5, 1));
     }
 
 
@@ -32,24 +29,26 @@ public class AttackSystem : MonoBehaviour
     /// <param name="objectToFace">Which object does it face.</param>
     /// <param name="timeToWait">Time before damage is applied.</param>
     /// <param name="sprite">Sprite and corresponding collider to use on floor marker.</param>
-    public IEnumerator FloorMarkerAttack(Vector3 spawnPosition, Vector3 objectToFace, float timeToWait, int sprite)
+    public IEnumerator FloorMarkerAttack(Vector3 spawnPosition, float timeToWait, int sprite)
     {
         markerRenderer.sprite = floorMarkers[sprite];
         usedCollider = attackColliders[sprite];
         floorMarkerObject.transform.position = spawnPosition;
 
-        objectToFace.x = 90;
-        objectToFace.z = 0;
-
-        floorMarkerObject.transform.LookAt(objectToFace);
-
         yield return new WaitForSeconds(timeToWait);
         markerRenderer.enabled = false;
 
-        if (usedCollider.bounds.Contains(boatObject.transform.position)) //Checks if boat is in trigger radius
+        if (isCancelled)
         {
-            Debug.Log("Hit");
-            //Damage function
+            isCancelled = false;
+        }
+        else 
+        {
+            if (usedCollider.bounds.Contains(boatObject.transform.position)) //Checks if boat is in trigger radius
+            {
+                Debug.Log("Hit");
+                //Damage function
+            }
         }
 
         floorMarkerObject.transform.position = storePosition;
